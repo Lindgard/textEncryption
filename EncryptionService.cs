7aa94b1b5416323e6900313fd95b7b305b40d4b8
pisani <a.b.lindgard@gmail.com>
2025-12-11 20:03:28 +0100

    removed static from the methods as they crashed the program after refactoring

--- EncryptionService.cs
+++ EncryptionService.cs
@@ -3,7 +3,7 @@ namespace textEncryption;
 
 public class EncryptionService
 {
-    public static string Encrypt(string text, byte key)
+    public string Encrypt(string text, byte key)
     {
         var chars = text.ToCharArray();
         for (int i = 0; i < chars.Length; i++)
@@ -13,12 +13,12 @@ public class EncryptionService
         return new string(chars);
     }
 
-    public static string Decrypt(string text, byte key)
+    public string Decrypt(string text, byte key)
     {
         return Encrypt(text, key);
     }
 
-    public static byte? TryParseKey(string? keyText)
+    public byte? TryParseKey(string? keyText)
     {
         if (byte.TryParse(keyText, out byte key))
             return key;
